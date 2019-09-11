508f2425e58680864fc2361731effbf5bd2f9172
Arkadiusz Chmura <arekchmura@gmail.com>
2019-09-11 20:41:16 +0200

    Add ViewControllers for event's pages

---
+++ DaysCounter/FutureEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FutureEventsViewController.swift
+//  DaysCounter
+//
+//  Created by Arkadiusz Chmura on 11/09/2019.
+//  Copyright © 2019 CloudDroid. All rights reserved.
+//
+
+import UIKit
+
+class FutureEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
