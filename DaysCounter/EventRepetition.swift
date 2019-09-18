e74826fe9d565194ec173d33026c636c3a897d12
Arkadiusz Chmura <arekchmura@gmail.com>
2019-09-18 17:41:11 +0200

    Add first step of adding an event
    First step consist of filling out details about a new event- name,
    date and time, notes, reminder etc. From that screen you go to the
    second step, where you choose a background and personalize your
    counter.

---
+++ DaysCounter/EventRepetition.swift
@@ -0,0 +1,13 @@
+//
+//  EventRepetition.swift
+//  DaysCounter
+//
+//  Created by Arkadiusz Chmura on 17/09/2019.
+//  Copyright © 2019 CloudDroid. All rights reserved.
+//
+
+import Foundation
+
+enum EventRepetition : Int {
+    case once, daily, weekly, monthly, yearly
+}
