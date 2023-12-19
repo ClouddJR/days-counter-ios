534ebc355cf707ee643d78781a3bdf8b8447367c
Arkadiusz Chmura <arekchmura@gmail.com>
2023-12-19 18:37:28 +0100

    Add an image and a stack view with buttons below
    Just a proof of concept. Probably it would be too much work to rewrite that screen to SwiftUI (and some things even seem impossible at the moment with the new framework).

--- DaysCounter/Form/New/CustomizationView.swift
+++ DaysCounter/Form/New/CustomizationView.swift
@@ -7,6 +7,10 @@ struct CustomizationView: View {
     @Binding var customization: CustomizationData
     
     var body: some View {
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        let ratio = min(screenWidth, screenHeight) / max(screenWidth, screenHeight)
+        
         ZStack {
             Image(uiImage: #imageLiteral(resourceName: "nature1.jpg"))
                 .resizable()
@@ -15,6 +19,38 @@ struct CustomizationView: View {
             
             VisualEffectView(effect: UIBlurEffect(style: .dark))
                 .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 15) {
+                Image(uiImage: #imageLiteral(resourceName: "nature1.jpg"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity
+                    )
+                    .aspectRatio(ratio, contentMode: .fit)
+                    .clipped()
+                
+                VStack(spacing: 20) {
+                    Button {
+                        // Open gallery chooser
+                    } label: {
+                        Text("Choose background")
+                            .font(.title3)
+                    }
+                    
+                    Button {
+                        // Open personaliation modal
+                    } label: {
+                        Text("Personalize")
+                            .font(.title3)
+                    }
+                }
+            }
+            .padding([.leading, .trailing], 20)
+            .padding([.top, .bottom], 15)
         }
     }
     
@@ -30,4 +66,11 @@ struct CustomizationView: View {
             // no-op
         }
     }
+}
+
+#Preview {
+    CustomizationView(
+        details: DetailsData(),
+        customization: .constant(CustomizationData())
+    )
 }
