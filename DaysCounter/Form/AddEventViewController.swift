b9ad09f1ce5fde0e549eb2a6596a9bb8d0ee518a
Arkadiusz Chmura <arekchmura@gmail.com>
2023-12-30 13:35:24 +0100

    Remove custom switch color

--- DaysCounter/Form/AddEventViewController.swift
+++ DaysCounter/Form/AddEventViewController.swift
@@ -5,11 +5,7 @@ final class AddEventViewController: UITableViewController {
     @IBOutlet weak var eventDateLabel: UILabel!
     @IBOutlet weak var eventDatePicker: UIDatePicker!
     @IBOutlet weak var entireDayCell: UITableViewCell!
-    @IBOutlet weak var entireDaySwitch: UISwitch! {
-        didSet {
-            entireDaySwitch.onTintColor = UIColor(red: 242/255, green: 132/255, blue: 91/255, alpha: 1.0)
-        }
-    }
+    @IBOutlet weak var entireDaySwitch: UISwitch!
     @IBOutlet weak var eventTimeCell: UITableViewCell!
     @IBOutlet weak var eventTimeLabel: UILabel!
     @IBOutlet weak var eventTimePicker: UIDatePicker!
@@ -18,11 +14,7 @@ final class AddEventViewController: UITableViewController {
     @IBOutlet weak var eventNotesTextView: UITextView!
     
     @IBOutlet weak var reminderSwitchCell: UITableViewCell!
-    @IBOutlet weak var reminderSwitch: UISwitch! {
-        didSet {
-            reminderSwitch.onTintColor = UIColor(red: 242/255, green: 132/255, blue: 91/255, alpha: 1.0)
-        }
-    }
+    @IBOutlet weak var reminderSwitch: UISwitch!
     @IBOutlet weak var reminderDateAndTimeCell: UITableViewCell!
     @IBOutlet weak var reminderDateAndTimeLabel: UILabel!
     @IBOutlet weak var reminderDateAndTimePicker: UIDatePicker!
