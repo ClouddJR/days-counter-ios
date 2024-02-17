106f80582ff64a61dd42517a18e4ba9160b34020
Arkadiusz Chmura <arekchmura@gmail.com>
2024-02-17 15:41:26 +0100

    Reformat file

--- DaysCounter/Supporting Files/AppDelegate.swift
+++ DaysCounter/Supporting Files/AppDelegate.swift
@@ -4,11 +4,12 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         changeDefaultRealmPath()
         FirebaseApp.configure()
         repeatEventsIfNecessary()
@@ -78,4 +79,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 }
-
