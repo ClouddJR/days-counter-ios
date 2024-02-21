e8fbabb0e4948fc555055e302a56984c41dd90f7
Arkadiusz Chmura <arekchmura@gmail.com>
2024-02-21 19:53:15 +0100

    Fetch events when the app goes back to foreground

--- DaysCounter/MainViewController.swift
+++ DaysCounter/MainViewController.swift
@@ -6,10 +6,41 @@ final class MainViewController: UINavigationController {
         styleNavigationBar()
         
         ImageMigrator().migrate()
-        DatabaseRepository().fetchEventsFromTheCloud()
+        fetchEvents()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.didBecomeActive),
+            name: UIApplication.didBecomeActiveNotification,
+            object: nil
+        )
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name:  UIApplication.didBecomeActiveNotification,
+            object: nil
+        )
     }
     
     private func styleNavigationBar() {
         navigationBar.shadowImage = UIImage()
+    }
+    
+    @objc func didBecomeActive(_ notification: Notification)  {
+        guard notification.name == UIApplication.didBecomeActiveNotification else { return }
+        
+        fetchEvents()
+    }
+    
+    private func fetchEvents() {
+        DatabaseRepository().fetchEventsFromTheCloud()
     }
 }
